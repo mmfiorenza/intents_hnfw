bfc0e17b2a872913891acba31cdabf1bfad9c112
Mauricio Fiorenza <mmmfiorenza@gmail.com>
2020-07-29 18:38:36 -0300

    sbseg_version

---
+++ README.md
@@ -0,0 +1,92 @@
+# Versão em português
+## Protótipo de validação
+Este repositório traz o protótipo desenvolvido como forma de validação para a proposta de arquitetura que visa balizar o desenvolvimento de softwares para gerência de firewalls em redes híbridas.
+
+## Instalação do software
+Faça o download do projeto utilizando git
+
+`git clone https://github.com/mmfiorenza/intent_translator.git`
+
+Acesse a pasta do projeto.
+
+Execute o script "prepares_environment.sh" para instalação das dependências
+
+`sudo bash utils/prepares_environment.sh`
+
+
+## Uso
+Execute os módulos tradutores utilizando o script “run_application.sh”
+
+```bash
+sudo bash utils/run_application.sh
+```
+
+Execute a API para recepção das intenções
+
+```bash
+python3.7 api.py
+```
+
+Utilize o metodo HTTP POST (por exemplo: comando curl) para enviar a intenção em NILE para a aplicação:
+
+```bash
+curl --data-binary "@intent.txt" -X POST http://localhost:5000
+```
+Exemplos das três intenções suportadas estão disponíveis na pasta “examples”.
+
+
+## Suporte
+Este software não possui nenhuma forma de suporte. Caso tenha alguma dúvida favor enviar um e-mail para mauriciofiorenza.aluno@unipampa.edu.br.
+
+
+## Creditos
+* Desenvolvimento: Maurício Fiorenza
+* Orientação: Diego Kreutz
+
+
+
+# English version
+## Validation prototype
+This repository presents the prototype developed as a form of validation for the proposed architecture that aims to guide the development of software for firewall management in hybrid networks.
+
+
+## Software installation
+Download the project using git
+
+`git clone https://github.com/mmfiorenza/intent_translator.git`
+
+Access the project folder.
+
+Run the "prepares_environment.sh" script to install the dependenciess
+
+`sudo bash utils/prepares_environment.sh`
+
+
+## Usage
+Run the translator modules using the script “run_application.sh”
+
+```bash
+sudo bash utils/run_application.sh
+```
+
+Run the API to receive intentions
+
+```bash
+python3.7 api.py
+```
+
+Use the HTTP POST method (for example: curl command) to send the intention in NILE to the application:
+
+```bash
+curl --data-binary "@intent.txt" -X POST http://localhost:5000
+```
+Examples of the three supported intentions are available in the “examples” folder.
+
+
+## Support
+This software does not have any form of support. If you have any questions please send an email to mauriciofiorenza.aluno@unipampa.edu.br.
+
+
+## Credits
+* Development: Maurício Fiorenza
+* Mentor: Diego Kreutz
---
+++ api.py
@@ -0,0 +1,271 @@
+from flask import Flask, request
+import re
+import socket
+import struct
+from nameko.standalone.rpc import ClusterRpcProxy
+from nameko import exceptions
+
+
+CONFIG = {'AMQP_URI': "amqp://guest:guest@localhost:5672"}
+intent_archive = "intent.txt"
+app = Flask(__name__)
+
+
+def cidr_to_netmask(cidr):
+    network, net_bits = cidr.split('/')
+    hots_bits = 32 - int(net_bits)
+    netmask = socket.inet_ntoa(struct.pack('!I', (1 << 32) - (1 << hots_bits)))
+    return network, netmask
+
+
+def get_line(word):
+    with open(intent_archive) as archive:
+        for line_num, l in enumerate(archive, 0):
+            if word in l:
+                return line_num
+        return False
+
+
+def is_valid_ip (ip):
+    m = re.match(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", ip)
+    return bool(m) and all(map(lambda n: 0 <= int(n) <= 255, m.groups()))
+
+
+def search_in_arq(value, path):
+    for line in open(path):
+        if value in line:
+            _, value = line.split()
+    return value
+
+
+def identify_value(tag, value):
+    tag_status_ok = False
+    if tag == 'from' or tag == 'to':
+        if is_valid_ip(value) or value == 'any':
+            tag_status_ok = True
+        if not tag_status_ok:
+            result = search_in_arq(value, 'conf/hosts_protocols.conf')
+            if is_valid_ip(result):
+                value = result
+                tag_status_ok = True
+        if not tag_status_ok:
+            print('function DNS')
+    elif tag == 'rule' or tag == 'for':
+        if not tag_status_ok:
+            value = search_in_arq(value, 'conf/hosts_protocols.conf')
+            tag_status_ok = True
+    if tag_status_ok:
+        return value
+    else:
+        return False
+
+
+def process_intent_acl(dict_intent, intent_type):
+    parameters = ['from', 'to', 'rule', 'apply']
+    flag = 0
+    text_return = 'Missing parameter for intent type ' + intent_type.upper() + ':'
+    for parameter in parameters:
+        # check if all parameters are available
+        if parameter in dict_intent:
+            if parameter == 'from' or parameter == 'to':
+                if "endpoint('" in dict_intent[parameter] and "')" in dict_intent[parameter]:
+                    value = re.search(r"'(.*)'", dict_intent[parameter]).group(1)
+                    dict_intent[parameter] = value
+                    result = identify_value(parameter, dict_intent[parameter])
+                    if not result:
+                        return 'Not possible translate parameter "' + parameter + ': ' + dict_intent[parameter] + '"'
+                    else:
+                        dict_intent[parameter] = result
+                elif "range('" in dict_intent[parameter] and "')" in dict_intent[parameter]:
+                    value = re.search(r"'(.*)'", dict_intent[parameter]).group(1)
+                    range, netmask = cidr_to_netmask(value)
+                    dict_intent[parameter] = range
+                    dict_intent[parameter+'_mask'] = netmask
+                else:
+                    return 'Syntax error in parameter: '+parameter
+            elif parameter == 'rule':
+                value = re.search(r"'(.*)'", dict_intent[parameter]).group(1)
+                if "block('" in dict_intent[parameter] and "')" in dict_intent[parameter]:
+                    dict_intent[parameter] = 'block'
+                elif "allow('" in dict_intent[parameter] and "')" in dict_intent[parameter]:
+                    dict_intent[parameter] = 'allow'
+                else:
+                    return 'Syntax error in parameter: "' +parameter+'". Use "allow" or "block."'
+                result = identify_value(parameter, value)
+                if not result:
+                    return 'Not possible translate parameter "' + parameter + ': ' + dict_intent[parameter] + '"'
+                else:
+                    dict_intent['traffic'] = result
+            elif parameter == 'apply':
+                if dict_intent['apply'] != 'insert' and dict_intent['apply'] != 'remove':
+                    return 'In parameter "apply" use "insert" or "remove"'
+        else:
+            flag = 1
+            text_return += ' ' + parameter + ','
+    print(dict_intent)
+    if flag == 0:
+        result_cisco = 'Cisco commands for ACL: \n'
+        result_iptables = 'IPTABLES commands for ACL: \n'
+        with ClusterRpcProxy(CONFIG) as rpc:
+            result_cisco = result_cisco + rpc.cisco_translator.intent_to_cisco(dict_intent)
+            result_iptables = result_iptables + rpc.iptables_translator.intent_to_iptables(dict_intent)
+        result = result_cisco + '\n\n\n' + result_iptables
+        return result
+    else:
+        return text_return
+
+
+def process_intent_nat11(dict_intent, intent_type):
+    parameters = ['from', 'to', 'for', 'apply']
+    flag = 0
+    text_return = 'Missing parameter for intent type ' + intent_type.upper() + ':'
+    for parameter in parameters:
+        # check if all parameters are available
+        if parameter in dict_intent:
+            if parameter == 'from' or parameter == 'to':
+                if "endpoint('" in dict_intent[parameter] and "')" in dict_intent[parameter]:
+                    value = re.search(r"'(.*)'", dict_intent[parameter]).group(1)
+                    if '-' in value:
+                        value, port = value.split('-')
+                        if is_valid_ip(value):
+                            dict_intent[parameter] = value
+                        else:
+                            result = identify_value(parameter, dict_intent[parameter])
+                            if not result:
+                                return 'Not possible translate parameter "' + parameter + ': ' + dict_intent[
+                                    parameter] + '"'
+                            else:
+                                dict_intent[parameter] = result
+                        dict_intent[parameter+'_port'] = port
+                    else:
+                        if is_valid_ip(value):
+                            dict_intent[parameter] = value
+                        else:
+                            result = identify_value(parameter, dict_intent[parameter])
+                            if not result:
+                                return 'Not possible translate parameter "' + parameter + ': ' + dict_intent[
+                                    parameter] + '"'
+                            else:
+                                dict_intent[parameter] = result
+                else:
+                    return 'Syntax error in parameter: '+parameter
+            elif parameter == 'for':
+                if "traffic('" in dict_intent[parameter] and "')" in dict_intent[parameter]:
+                    value = re.search(r"'(.*)'", dict_intent[parameter]).group(1)
+                    dict_intent['protocol'] = value
+                else:
+                    return 'Syntax error in parameter: ' + parameter
+        else:
+            flag = 1
+            text_return += ' ' + parameter + ','
+    print(dict_intent)
+    if flag == 0:
+        result_cisco = 'Cisco commands for NAT11: \n'
+        result_iptables = 'IPTABLES commands for NAT11: \n'
+        with ClusterRpcProxy(CONFIG) as rpc:
+            result_cisco = result_cisco + rpc.cisco_translator.intent_to_cisco(dict_intent)
+            result_iptables = result_iptables + rpc.iptables_translator.intent_to_iptables(dict_intent)
+        result = result_cisco + '\n\n\n' + result_iptables
+        return result
+    else:
+        return text_return
+
+
+def process_intent_traffic_shaping(dict_intent, intent_type):
+    parameters = ['from', 'to', 'for', 'with', 'apply']
+    flag = 0
+    text_return = 'Missing parameter for intent type ' + intent_type.upper() + ':'
+    for parameter in parameters:
+        # check if all parameters are available
+        if parameter in dict_intent:
+            if parameter == 'from' or parameter == 'to':
+                if "endpoint('" in dict_intent[parameter] and "')" in dict_intent[parameter]:
+                    value = re.search(r"'(.*)'", dict_intent[parameter]).group(1)
+                    dict_intent[parameter] = value
+                    result = identify_value(parameter, dict_intent[parameter])
+                    if not result:
+                        return 'Not possible translate parameter "' + parameter + ': ' + dict_intent[parameter] + '"'
+                    else:
+                        dict_intent[parameter] = result
+                elif "range('" in dict_intent[parameter] and "')" in dict_intent[parameter] and "/" in dict_intent[parameter]:
+                    value = re.search(r"'(.*)'", dict_intent[parameter]).group(1)
+                    range, netmask = cidr_to_netmask(value)
+                    dict_intent[parameter] = range
+                    dict_intent[parameter + '_mask'] = netmask
+                else:
+                    return 'Syntax error in parameter: ' + parameter
+            elif parameter == 'for':
+                if "traffic('" in dict_intent[parameter] and "')" in dict_intent[parameter]:
+                    value = re.search(r"'(.*)'", dict_intent[parameter]).group(1)
+                else:
+                    return 'Syntax error in parameter: ' + parameter
+                result = identify_value(parameter, value)
+                if not result:
+                    return 'Not possible translate parameter "' + parameter + ': ' + dict_intent[parameter] + '"'
+                else:
+                    dict_intent['traffic'] = result
+            elif parameter == 'with':
+                if "throughput('" in dict_intent[parameter] and "')" in dict_intent[parameter]:
+                    value = re.search(r"'(.*)'", dict_intent[parameter]).group(1)
+                    if 'mbps' in value.lower():
+                        dict_intent[parameter] = int(re.search(r'\d+', value).group())
+                    else:
+                        return 'Use "Mbps" in throughout'
+                else:
+                    return 'Syntax error in parameter: "'+parameter
+        else:
+            flag = 1
+            text_return += ' ' + parameter + ','
+    print(dict_intent)
+    if flag == 0:
+        result_cisco = 'Cisco commands for Traffic Shaping: \n'
+        result_iptables = 'IPTABLES commands for Traffic Shaping: \n'
+        with ClusterRpcProxy(CONFIG) as rpc:
+            result_cisco = result_cisco + rpc.cisco_translator.intent_to_cisco(dict_intent)
+            result_iptables = result_iptables + rpc.iptables_translator.intent_to_iptables(dict_intent)
+        result = result_cisco + '\n\n\n' + result_iptables
+        return result
+    else:
+        return text_return
+
+
+def process_intent(intent):
+    try:
+        intent_type = re.search(r'define intent (.*):', str(intent)).group(1)
+    except AttributeError:
+        return "Syntax error in Intent"
+    with open(intent_archive, 'w+b') as archive:
+        archive.write(intent)
+    archive = open(intent_archive, 'r')
+    tmp_intent = archive.readlines()[get_line('define intent'):]
+    final_intent = {}
+    final_intent['name'] = intent_type.lower()
+    for line in tmp_intent[1:]:
+        try:
+            key, value = line.split()
+            key = key.lower()
+            final_intent[key] = value.lower()
+        except ValueError:
+            return 'Incomplete intent for type "'+intent_type.upper()+'", see /help'
+    print(final_intent)
+    if intent_type == 'acl':
+        return process_intent_acl(final_intent, intent_type)
+    elif intent_type == 'nat11':
+        return process_intent_nat11(final_intent, intent_type)
+    elif intent_type == 'traffic_shaping':
+        return process_intent_traffic_shaping(final_intent, intent_type)
+    else:
+        return "Unrecognized intent type, see /help"
+
+
+@app.route('/', methods=['POST'])
+def receive_intent():
+    request.get_data()
+    response = process_intent(request.data)
+    return response+"\n", 200
+
+
+if __name__ == '__main__':
+    app.run()
+
+
