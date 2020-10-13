fdb6e3cee32c3173b116b0ba56b7bd6bc3d48693
Mauricio Fiorenza <mmmfiorenza@gmail.com>
2020-10-13 18:53:03 -0300

    att utils

---
+++ utils/run_application.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+####
+# This script runs microservices and python api
+# 
+# Author: Maurício Fiorenza
+# Last modification: 04-10-2020
+####
+
+source venv_firewall/bin/activate
+
+cd services/cisco/
+nameko run cisco --broker amqp://guest:guest@localhost &
+
+cd ../../services/iptables/
+nameko run iptables --broker amqp://guest:guest@localhost &
+
+cd ../../connectors/
+nameko run ssh_connector --broker amqp://guest:guest@localhost &
+
+#python api.py
+
+echo "To send intents see README.MD"
